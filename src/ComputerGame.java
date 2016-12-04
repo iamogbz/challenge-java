155941f35fb0d4b619a111096f292d7847fc701a
Emmanuel Ogbizi-Ugbe <virgoe9@yahoo.com>
2016-12-04 23:09:55 +0100

    use input

--- src/ComputerGame.java
+++ src/ComputerGame.java
@@ -31,23 +31,23 @@ public class ComputerGame {
     public static void main(String args[]) throws IOException {
         try {
             // sample result - 1867
-            FileInputStream is = new FileInputStream(new File("computer-game.txt"));
+            FileInputStream is = new FileInputStream(new File("inputs/computer-game.txt"));
             System.setIn(is);
         } catch (FileNotFoundException ex) {
             logger.log(Level.SEVERE, null, ex);
         }
         long start = System.currentTimeMillis();
         BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
-        //int n = Integer.parseInt(in.readLine());
-        int n = 100000;
+        int n = Integer.parseInt(in.readLine());
+        //int n = 100000;
         String[] n1 = in.readLine().split("\\s");
         String[] n2 = in.readLine().split("\\s");
         logger.log(Level.INFO, "{0}ms for handling input", (System.currentTimeMillis() - start));
         start = System.currentTimeMillis();
         Integer num;
         for (int i = 0; i < n; i++) {
-            //num = Integer.parseInt(n1[i]);
-            num = ThreadLocalRandom.current().nextInt(800000000, 1000000000);
+            num = Integer.parseInt(n1[i]);
+            //num = ThreadLocalRandom.current().nextInt(800000000, 1000000000);
             Set<Integer> primeFactors = primeFactors(num);
             for (Integer f : primeFactors) {
                 if (!factored1.containsKey(f))
@@ -58,8 +58,8 @@ public class ComputerGame {
         logger.log(Level.INFO, "{0}ms for processing primes 1", (System.currentTimeMillis() - start));
         start = System.currentTimeMillis();
         for (int i = 0; i < n; i++) {
-            //num = Integer.parseInt(n2[i]);
-            num = ThreadLocalRandom.current().nextInt(800000000, 1000000000);
+            num = Integer.parseInt(n2[i]);
+            //num = ThreadLocalRandom.current().nextInt(800000000, 1000000000);
             Set<Integer> primeFactors = primeFactors(num);
             for (Integer f : primeFactors) {
                 if (!factored2.containsKey(f))
@@ -132,7 +132,7 @@ public class ComputerGame {
     // TODO OPTIMIZE THIS - CACHE RESULTS;
     private static Set<Integer> primeFactors(int n) {
         final Set<Integer> primes = new HashSet<>();
-        // Print the number of 2s that divide n
+        // save the number of 2s that divide n
         while (n % 2 == 0) {
             primes.add(2);
             n /= 2;
@@ -140,7 +140,7 @@ public class ComputerGame {
         // n must be odd at this point.  So we can
         // skip one element (Note i = i +2)
         for (int i = 3; i <= Math.sqrt(n); i += 2)
-            // While i divides n, print i and divide n
+            // While i divides n, save i and divide n
             while (n % i == 0) {
                 primes.add(i);
                 n /= i;
