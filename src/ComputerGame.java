b77b26e29d46a55b3bd2b54eb26117700fe52f03
Emmanuel Ogbizi-Ugbe <virgoe9@yahoo.com>
2016-12-10 05:19:09 +0100

    parsing input at time of read

--- src/ComputerGame.java
+++ src/ComputerGame.java
@@ -5,7 +5,6 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.nio.charset.Charset;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
@@ -19,8 +18,7 @@ public class ComputerGame {
     final private static Logger logger = Logger.getLogger("ComputerGame");
     final private static DebugTimer timer = new DebugTimer();
     private static BufferedReader in;
-    private static Integer n, num, pi, vn, source, dest, result;
-    private static String[] n1, n2;
+    private static Integer n, pi, vn, source, dest, result;
     private static Map<Integer, Integer> primeNodes;
     private static List<Dinic.Edge>[] graph;
 
@@ -33,23 +31,29 @@ public class ComputerGame {
             logger.log(Level.SEVERE, null, ex);
             return;
         }
+        String[] s;
+        int[] n1, n2;
         timer.start();
-
         in = new BufferedReader(new InputStreamReader(System.in));
         n = Integer.parseInt(in.readLine());
-        n1 = in.readLine().split("\\s");
-        n2 = in.readLine().split("\\s");
+        n1 = new int[n];
+        s = in.readLine().split("\\s");
+        for (int i = 0; i < n; i++)
+            n1[i] = Integer.parseInt(s[i]);
+        n2 = new int[n];
+        s = in.readLine().split("\\s");
+        for (int i = 0; i < n; i++)
+            n2[i] = Integer.parseInt(s[i]);
         timer.lap("reading input");
 
         source = 0;
         dest = 1;
         primeNodes = new HashMap<>();
-        graph = Dinic.createGraph(400000); // assume upper limit of nodes
+        graph = Dinic.createGraph(300000);
         for (int i = 0; i < n; i++) {
             vn = i + 2;
             Dinic.addEdge(graph, source, vn, 1);
-            num = Integer.parseInt(n1[i]);
-            for (Integer p : primeFactors(num)) {
+            for (Integer p : primeFactors(n1[i])) {
                 pi = primeNodes.get(p);
                 if (pi == null) {
                     pi = n * 2 + primeNodes.size() + 1;
@@ -61,8 +65,7 @@ public class ComputerGame {
         for (int i = 0; i < n; i++) {
             vn = n + i + 2;
             Dinic.addEdge(graph, vn, dest, 1);
-            num = Integer.parseInt(n2[i]);
-            for (Integer p : primeFactors(num)) {
+            for (Integer p : primeFactors(n2[i])) {
                 pi = primeNodes.get(p);
                 if (pi != null)
                     Dinic.addEdge(graph, pi, vn, 1);
@@ -81,23 +84,23 @@ public class ComputerGame {
     private static Set<Integer> primeFactors(int n) {
         final Set<Integer> primes = new HashSet<>();
         // While 2 divides n, add 2 and divide n
-        while (n % 2 == 0) {
-            primes.add(2);
-            n /= 2;
-        }
+            while (n % 2 == 0) {
+                primes.add(2);
+                n /= 2;
+            }
         // n must be odd at this point.  So we can
         // skip one element (Note i = i +2)
         for (int i = 3; i <= Math.sqrt(n); i += 2)
             // While i divides n, save i and divide n
             while (n % i == 0) {
                 primes.add(i);
-                n /= i;
+                    n /= i;
             }
 
         // This condition is to handle the case when
         // n is a prime number greater than 2
-        if (n > 2)
-            primes.add(n);
+            if (n > 2)
+                primes.add(n);
         return primes;
     }
 
